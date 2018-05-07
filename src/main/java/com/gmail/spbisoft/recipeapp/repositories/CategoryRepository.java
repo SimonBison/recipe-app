63c59c2420ffbefc0caf2f8e343e16bcc9ad694e
sbi <szymon.bizon@hicron.com>
2018-05-07 14:23:31 +0200

    Created Spring Data JPA Query Methods.

--- src/main/java/com/gmail/spbisoft/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/gmail/spbisoft/recipeapp/repositories/CategoryRepository.java
@@ -3,6 +3,10 @@ package com.gmail.spbisoft.recipeapp.repositories;
 import com.gmail.spbisoft.recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 
 }
